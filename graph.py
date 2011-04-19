727021eb54bbe244c69081d565af4e7e67b985b6
Anand Patil <anand.prabhakar.patil@gmail.com>
2011-04-19 18:15:44 +0100

    Cleanup

--- graph.py
+++ graph.py
@@ -1,9 +1,14 @@
 from stripackd import trmesh, trlist
 import stripackd
 
+__all__ = ['triangulate_sphere', 'plot_triangulation']
+
+def fortan_index(a, i):
+    return a[i-1]
+    
 def trilist2trimap(n, trilist):
     "Converts the list of triangles to a convenient node-to-triangle map."
-    trimap = dict([(i,[]) for i in xrange(n)])
+    trimap = [[] for i in xrange(n)]
     for t in trilist:
         for i in t:
             trimap[i].append(t)
@@ -18,18 +23,19 @@ def neighbors2tri(n, neighbors):
             if j > i:
                 shared_endpts = np.array(list(i_edges & neighbors[j]))
                 triangles = triangles + [(i,j,s) for s in shared_endpts[np.where(shared_endpts>j)]]
-                
-    return triangles, trilist2trimap(n, triangles)
 
-def fortan_index(a, i):
-    return a[i-1]
+    return triangles, trilist2trimap(n, triangles)
 
 def triangulate_sphere(X):
     """
     X are the x,y,z coordinates of the points to triangulate on the unit sphere.
 
-    Returns: Map from vertex to neigbors, list of triangles, map from vertex to all adjacent triangles.
+    Returns: 
+        - List of neighbors of each vertex
+        - List of triangles represented as tuples of three indices
+        - List of all triangles adjacent to each vertex.
     """
+    
     x,y,z = X.T
     lst, lptr, lend, lnew, ier = trmesh(x,y,z)
     if ier != 0:
@@ -70,21 +76,18 @@ def plot_triangulation(X,neighbors):
             ax.plot([x[frm], x[to]], [y[frm], y[to]], [z[frm], z[to]], 'k-')
             
     ax.plot(x,y,z,'r.')
-
-def get_neighboring_triangles(index, vert):
-    pass
     
     
 if __name__ == '__main__':
     import numpy as np
     import pymc as pm
     
-    n = 10
+    n = 500
     X = np.random.normal(size=(3,n))
     # X[0,:] = X[0,:]**2
     X /= np.sqrt((X**2).sum(axis=0))
     
     neighbors, triangles, trimap = triangulate_sphere(X.T)
-    plot_triangulation(X.T,neighbors)
+    # plot_triangulation(X.T,neighbors)
     
     # plot_triangulation(x,y,z)
