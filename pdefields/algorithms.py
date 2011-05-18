7656f20db1edc9f77d57ac01c16a3f0b85511f53
Anand Patil <anand@tantarakatin.(none)>
2011-05-18 16:03:56 +0100

    Factored out on-the-fly compilation

--- pdefields/algorithms.py
+++ pdefields/algorithms.py
@@ -36,6 +36,35 @@ def eta(M,Q):
      """
     return Q*M
 
+def compile_fortran_routine(fortran_likelihood_code, modname, template):
+    "Used by compile_metropolis_sweep and compile_EP_sweep."
+    def fortran_indent(s):
+        return '\n'.join([' '*6+l for l in s.splitlines()])
+    
+    # Instantiate the template with the likelihood code snippet.
+    fortran_likelihood_code = fortran_likelihood_code.replace('{X}','(xp+M(i))')
+    
+    fortran_code = template.replace('{LIKELIHOOD_CODE}', fortran_indent(fortran_likelihood_code))
+    lpf_hash = hashlib.sha1(fortran_code).hexdigest()
+    
+    # Compile the generated code and import it.
+    try:
+        exec('import %s_%s as %s'%(modname, lpf_hash, modname))
+    except ImportError:
+        for l in fortran_likelihood_code.splitlines():
+            if len(l)>72-6:
+                raise RuntimeError, 'Line "%s" in your log-likelihood code snippet is too long, Fortran will hurl.'%l
+                
+        f2py.compile(fortran_code, modulename='%s_%s'%(modname,lpf_hash))
+    exec('import %s_%s as %s'%(modname, lpf_hash, modname))
+
+    # Store the generated code on the function and return it.
+    exec('%s.code = fortran_code'%modname)
+    exec('mod = %s'%modname)
+    return mod
+    
+    
+
 def compile_metropolis_sweep(fortran_likelihood_code):
     """
     Takes an unindented template of Fortran code with the following template parameters:
@@ -50,29 +79,8 @@ def compile_metropolis_sweep(fortran_likelihood_code):
     
     Returns a function for use by fast_metropolis_sweep. The generated code is included in the return object as the 'code' attribute.
     """
-    def fortran_indent(s):
-        return '\n'.join([' '*6+l for l in s.splitlines()])
-    
-    # Instantiate the template with the likelihood code snippet.
-    fortran_likelihood_code = fortran_likelihood_code.replace('{X}','(xp+M(i))')
-    fortran_code = file(os.path.join(os.path.split(pdefields.__file__)[0],'fast_metro.f')).read()
-    fortran_code = fortran_code.replace('{LIKELIHOOD_CODE}', fortran_indent(fortran_likelihood_code))
-    lpf_hash = hashlib.sha1(fortran_code).hexdigest()
-    
-    # Compile the generated code and import it.
-    try:
-        exec('import gmrf_metro_%s as gmrf_metro'%lpf_hash)
-    except ImportError:
-        for l in fortran_likelihood_code.splitlines():
-            if len(l)>72-6:
-                raise RuntimeError, 'Line "%s" in your log-likelihood code snippet is too long, Fortran will hurl.'%l
-                
-        f2py.compile(fortran_code, modulename='gmrf_metro_%s'%lpf_hash)
-    exec('import gmrf_metro_%s as gmrf_metro'%lpf_hash)
-    
-    # Store the generated code on the function and return it.
-    gmrf_metro.code = fortran_code
-    return gmrf_metro
+    template = file(os.path.join(os.path.split(pdefields.__file__)[0],'fast_metro.f')).read()
+    return compile_fortran_routine(fortran_likelihood_code, 'gmrf_metro', template)
 
 def fast_metropolis_sweep(M,Q,gmrf_metro,x,likelihood_variables=None,n_sweeps=10):
     """
@@ -117,7 +125,6 @@ def fast_metropolis_sweep(M,Q,gmrf_metro,x,likelihood_variables=None,n_sweeps=10
 def spmatvec(m,v):
     return (m*v.reshape((-1,1))).view(np.ndarray).ravel()
 
-# TODO: Also make an EP version. Should take a Fortran code snippet.
 def scoring_gaussian_full_conditional(M,Q,pattern_products,like_deriv1,like_deriv2,backend,tol):
     """
     This function produces an approximate Gaussian full conditional for a multivariate normal variable x with sparse precision Q. This can be used to produce an approximate MCMC scheme or an INLA-like scheme.
@@ -145,4 +152,33 @@ def scoring_gaussian_full_conditional(M,Q,pattern_products,like_deriv1,like_deri
         
         x = x + delta
     
+    return x, precision_products
+    
+def EP_gaussian_full_conditional(M,Q,fortran_likelihood_code,tol):
+    """
+    This function produces an approximate Gaussian full conditional for a multivariate normal variable x with sparse precision Q. This can be used to produce an approximate MCMC scheme or an INLA-like scheme.
+
+    The scoring algorithm is described in section 2.2 of Rue, Martino and Chopin.
+
+    Takes:
+    - Mean vector M
+    - Sparse precision matrix Q, in SciPy CSC or CSR format
+    - pattern_products: The backend's precomputations based on sparsity pattern alone.
+    - like_deriv1: A function that takes a value for x and returns an array whose i'th element is the derivative of the likelihood of datapoint i with respect to x[i].
+    - like_deriv2: Same, but the second derivative.
+    - The linear algebra backend that will handle the matrix solves.
+
+    Returns the approximate full conditional mean of x, the backend's analysis of the full conditional precision Q, and the approximate evidence (the probability of the data conditional on M and Q, not x).
+    """
+    x = M
+    delta = x*0+np.inf
+    while np.abs(delta).max() > tol:
+        # print np.abs(delta).max()
+        d1 = like_deriv1(x)
+        d2 = like_deriv2(x)
+        grad2 = d1-spmatvec(Q,(x-M))
+        delta, precision_products = backend.precision_solve_v(Q,d1-spmatvec(Q,(x-M)),-d2,**pattern_products)
+
+        x = x + delta
+
     return x, precision_products
